7ea956771c6dd7fda87d059d71649ee2f6d21422
junjie <1559304351@qq.com>
2023-06-13 15:29:50 +0800

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
+module.exports = {
+  content: [
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
